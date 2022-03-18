ab4a86c8968be8795b108212c8fc339bb95f04c6
rickycheng31 <99989598+rickycheng31@users.noreply.github.com>
2022-03-17 20:53:09 -0400

    js

---
+++ client/lab_4/script.js
@@ -0,0 +1,7 @@
+const first_item = document.querySelector("carousel-item.visible");
+const last_item = document.querySelector("carousel-item:last-of-type")
+document.
+    querySelector('next_button')
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    });
