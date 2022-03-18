670b0ca82811ec299a9a017d4e59ce69eb51dfa1
rickycheng31 <99989598+rickycheng31@users.noreply.github.com>
2022-03-17 23:10:41 -0400

    button remapping

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,7 +1,6 @@
-const first_item = document.querySelector("carousel-item.visible");
-const last_item = document.querySelector("carousel-item:last-of-type")
-document.
-    querySelector('next_button')
-    .addEventListener("click", function() {
-        moveToNextSlide();
-    });
+const first = document.querySelector(".carousel-item.visible");
+const last = document.querySelector(".carousel-item:last-of-type");
+
+let current = first;
+
+document.querySelector()
